e8d71fb0b6e82a6820f8fca149791561bccddf32
Beatrizvn <beatriz-vn@hotmail.com>
2024-09-08 12:16:44 -0300

    verifica se pode usar

--- jogo/Tela/equipados.py
+++ jogo/Tela/equipados.py
@@ -1,4 +1,6 @@
 from Database.Dml.equipados import atualizar_equipamento
+from Database.Select.jogador import info_jogador
+from Classes.jogador import Jogador
 from uteis import clear_screen
 from Database.Select.equipados import equipaveis, info_equipados
 
@@ -6,7 +8,6 @@ def equipados(conn, id_jogador):
     while True:
         clear_screen()
         equipados = info_equipados(conn, id_jogador)
-
         print(f'1. Armadura: {equipados[3]}')
         print(f'2. Mão direita: {equipados[1]}')
         print(f'3. Mão esquerda: {equipados[2]}')
@@ -33,26 +34,32 @@ def equipados(conn, id_jogador):
 def listar_equipaveis(conn, id_jogador, tipo_equipamento, equipados):
     while True:
         # clear_screen()
+        jogador_info = info_jogador(conn, id_jogador)
+        if jogador_info:
+            jogador = Jogador.from_data_base(jogador_info)
+
+        print(f"---------------------------------")
+        print(f"Seus atributos: Inteligencia: {jogador.intel} Força: {jogador.forca} Fé: {jogador.fe} Destreza: {jogador.destreza}")
         listar_equipaveis = equipaveis(conn, id_jogador, tipo_equipamento)
         if tipo_equipamento == 'Armadura':
             for i, itens in enumerate(listar_equipaveis, start=1):
                 print(f"{i}. {itens[1]} "
                     f"{f'(Resistência = {itens[3]}) ' if itens[3] else ''}"
                     f"Requisitos: INT: {itens[4]}, FORÇA: {itens[5]}, FÉ: {itens[6]}, DEX: {itens[7]}"
-                    f"{'--Equipado--' if itens[8] == equipados[6] else ''}")
+                    f"{' --Equipado--' if itens[8] == equipados[6] else ''}")
         elif tipo_equipamento == 'MaoE':
             for i, itens in enumerate(listar_equipaveis, start=1):
                 print(f"{i}. {itens[1]} "
                     f"{f'(Defesa = {itens[7]}) ' if itens[3] else ''}"
                     f"Requisitos: INT: {itens[3]}, FORÇA: {itens[4]}, FÉ: {itens[5]}, DEX: {itens[6]}"
-                    f"{'--Equipado--' if itens[9] == equipados[5] else ''}")
+                    f"{' --Equipado--' if itens[9] == equipados[5] else ''}")
         elif tipo_equipamento == 'MaoD':
             for i, itens in enumerate(listar_equipaveis, start=1):
                 print(f"{i}. {itens[1]} "
                     f"{f'(Dano = {itens[7]}) ' if itens[7] else ''}"  
                     f"Requisitos: INT: {itens[3]}, FORÇA: {itens[4]}, FÉ: {itens[5]}, DEX: {itens[6]} "
                     f"(Habilidade Dano = {itens[8]})"
-                    f"{'--Equipado--' if itens[9] == equipados[4] else ''}")
+                    f"{' --Equipado--' if itens[9] == equipados[4] else ''}")
         else:
             print("Tipo de equipamento inválido.")
 
@@ -62,8 +69,9 @@ def listar_equipaveis(conn, id_jogador, tipo_equipamento, equipados):
         if opcao == "0":
             break
         else:
-            if atualiza_equipamento(conn, id_jogador, tipo_equipamento, opcao, listar_equipaveis): break
-
+            if verifica_se_pode_usar(jogador, opcao, listar_equipaveis, tipo_equipamento): 
+                if atualiza_equipamento(conn, id_jogador, tipo_equipamento, opcao, listar_equipaveis): break
+            else: print("Você não pode usar esse equipamento. Verifique se você atende aos requisitos.")
 
 def atualiza_equipamento(conn, id_jogador, tipo_equipamento, opcao, listar_equipaveis):
     try:
@@ -83,3 +91,20 @@ def atualiza_equipamento(conn, id_jogador, tipo_equipamento, opcao, listar_equip
             print("Opção inválida.")
     except ValueError:
         print("Por favor, insira um número válido.")
+
+def verifica_se_pode_usar(jogador, opcao, listar_equipaveis, tipo_equipamento):
+    try:
+        opcao = int(opcao)
+        if 1 <= opcao <= len(listar_equipaveis):
+            equipamento_selecionado = listar_equipaveis[opcao - 1]
+            if tipo_equipamento == "Armadura" and (jogador.intel >= equipamento_selecionado[4] and jogador.forca >= equipamento_selecionado[5] and jogador.fe >= equipamento_selecionado[6] and jogador.destreza >= equipamento_selecionado[7]):
+                return True
+            elif tipo_equipamento != "Armadura" and (jogador.intel >= equipamento_selecionado[3] and jogador.forca >= equipamento_selecionado[4] and jogador.fe >= equipamento_selecionado[5] and jogador.destreza >= equipamento_selecionado[6]):
+                return True
+            else:    
+                return False
+        else:
+            print("Opção inválida.")
+    except ValueError:
+        print("Por favor, insira um número válido.")
+    return False
